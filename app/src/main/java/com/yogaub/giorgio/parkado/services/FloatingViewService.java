156c40f4b67e1dc0b4ce0887a26da811c9cc0677
ClonedOne <severi.giorgio@gmail.com>
2017-01-17 22:40:30 +0100

    Initial commit with floating view

---
+++ app/src/main/java/com/yogaub/giorgio/parkado/services/FloatingViewService.java
@@ -0,0 +1,137 @@
+package com.yogaub.giorgio.parkado.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.yogaub.giorgio.parkado.R;
+
+
+/**
+ * Created by Giorgio on yogaub.
+ *
+ * This class is a service responsible for the creation and management of the Chat Head.
+ */
+
+public class FloatingViewService extends Service {
+
+    private WindowManager windowManager;
+    private View FloatingView;
+    private WindowManager.LayoutParams params;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FloatingView = LayoutInflater.from(this).inflate(R.layout.floating_view, null);
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        //The root element of the collapsed view layout
+        final View collapsedView = FloatingView.findViewById(R.id.collapse_view);
+        //The root element of the expanded view layout
+        final View expandedView = FloatingView.findViewById(R.id.expanded_container);
+
+        params = new WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        );
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        FloatingView.setOnTouchListener(new View.OnTouchListener(){
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        int Xdiff = (int) (event.getRawX() - initialTouchX);
+                        int Ydiff = (int) (event.getRawY() - initialTouchY);
+
+
+                        //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
+                        //So that is click event.
+                        if (Xdiff < 10 && Ydiff < 10) {
+                            if (isViewCollapsed()) {
+                                //When user clicks on the image view of the collapsed layout,
+                                //visibility of the collapsed layout will be changed to "View.GONE"
+                                //and expanded view will become visible.
+                                collapsedView.setVisibility(View.GONE);
+                                expandedView.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(FloatingView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        windowManager.addView(FloatingView, params);
+
+        //Set the close button
+        ImageView closeButtonCollapsed = (ImageView) FloatingView.findViewById(R.id.close_btn);
+        closeButtonCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close the service and remove the from from the window
+                stopSelf();
+            }
+        });
+
+        //Set the close button
+        ImageView closeButton = (ImageView) FloatingView.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+            }
+        });
+
+
+    }
+
+    private boolean isViewCollapsed() {
+        return FloatingView == null || FloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (FloatingView != null) windowManager.removeView(FloatingView);
+    }
+
+}
