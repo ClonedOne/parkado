500b8af62810495cc97458fe13e4c2fc26618205
ClonedOne <severi.giorgio@gmail.com>
2017-01-21 00:52:45 +0100

    Added codepark

--- app/src/main/java/com/yogaub/giorgio/parkado/MainActivity.java
+++ app/src/main/java/com/yogaub/giorgio/parkado/MainActivity.java
@@ -1,16 +1,11 @@
 package com.yogaub.giorgio.parkado;
 
-import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -52,46 +47,7 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-        /* Code to be reused elsewhere
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(Constants.DBG_CHATHEAD, "Permission is not granted");
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SYSTEM_ALERT_WINDOW)) {
-                Log.d(Constants.DBG_CHATHEAD, "Permission was denied in the past, should show rationale");
-                showMessageOKCancel(getString(R.string.perm_chathead), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.SYSTEM_ALERT_WINDOW}, 123);
-                    }
-                });
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.SYSTEM_ALERT_WINDOW}, 123);
-            }
-        }
-        */
-
     }
-
-    /* Code to be reused elsewhere
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case Constants.CHATHEAD_PERMISSION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    Log.d(Constants.DBG_CHATHEAD, "User granted permission");
-                    Intent intent = new Intent(this, FloatingViewService.class);
-                    startService(intent);
-                } else {
-                    Log.d(Constants.DBG_CHATHEAD, "User denied permission");
-                    Toast denied_t = Toast.makeText(this, getString(R.string.perm_chathead_denied), Toast.LENGTH_LONG);
-                    denied_t.show();
-                }
-
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-    */
 
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener, DialogInterface.OnClickListener cancelListener) {
         new AlertDialog.Builder(MainActivity.this)
